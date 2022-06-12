4e340b57dee61cc71a3e8ca75bc731ba5e2c6b00
SANG-A <blossom78325@gmail.com>
2022-06-12 12:19:53 +0900

    feat: connect post api to application

--- server/app.js
+++ server/app.js
@@ -1,10 +1,11 @@
-import express from "express";
-import mongoose from "mongoose";
-import config from "./config";
-import helmet from "helmet";
-import hpp from "hpp";
-import cors from "cors";
-import morgan from "morgan";
+import express from 'express';
+import mongoose from 'mongoose';
+import config from './config';
+import helmet from 'helmet';
+import hpp from 'hpp';
+import cors from 'cors';
+import morgan from 'morgan';
+import postsRoutes from './routes/api/post';
 
 const app = express();
 const {MONGO_URI} = config;
@@ -16,17 +17,18 @@ app.use(helmet());
 // 보안 - secure your Express apps by setting various HTTP headers.
 app.use(cors({origin: true, credential: true}));
 // CORS - a node.js package for providing a Connect/Express middleware that can be used to enable CORS with various options.
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 // Log - HTTP request logger middleware for node.js
 app.use(express.json());
 // 브라우저에서 내용을 보내면 서버에서 json 형태로 해석한다.
 
 mongoose.connect(MONGO_URI)
-	.then(() => console.log("MongoDB connecting Success!!"))
+	.then(() => console.log('MongoDB connecting Success!!'))
 	.catch((e) => console.log({e}));
 
 // Use routes
-app.get("/");
+app.get('/');
 // '/'는 home이라는 뜻이다.
+app.use('/api/post', postsRoutes);
 
 export default app;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 import app from "./app";
 
-app.listen("3333", () => {
-	console.log('hi');
+app.listen("3000", () => {
+	console.log('App is listening');
 })
