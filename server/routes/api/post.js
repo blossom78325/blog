5ecdf79bc723d5d1ef16362efecc647f184df09f
SANG-A <blossom78325@gmail.com>
2022-06-12 23:29:20 +0900

    feat: add validation on creating post

--- server/routes/api/post.js
+++ server/routes/api/post.js
@@ -9,10 +9,15 @@ router.get('/', async (req, res) => {
 	res.json(postFindResult);
 });
 
-router.post('/', async (req, res, next) => {
+router.post('/', async (req, res) => {
 	try {
 		console.log({req});
 		const {title, contents, fileUrl, creator} = req.body;
+
+		if (!title || !contents) {
+			return res.status(400).json({msg: '필수 항목을 모두 입력해주세요.'});
+		}
+
 		const newPost = await Post.create({
 			title, contents, fileUrl, creator,
 		});
