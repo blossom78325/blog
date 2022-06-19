ec07aeacfceccda9ee6998cf5845fbd963ba2758
김상아 Sang-A KIm <blossom78325@gmail.com>
2022-06-19 19:22:19 +0900

    fix: correct to make password hash

--- server/routes/api/user.ts
+++ server/routes/api/user.ts
@@ -38,29 +38,34 @@ router.post('/', async (req, res) => {
 
     User.findOne({email}).then(user => {
       if (user) return res.status(400).json({msg: '이미 가입된 계정입니다.'});
-    });
 
-    const newUser = new User({name, email, password});
+      const newUser = new User({name, email, password});
 
-    // salt: a salt is random data that is used as an additional input to a one-way function that hashes data, a password or passphrase.
-    bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash(newUser.password, salt, (err, hash) => {
-        if (err) throw err;
+      // salt: a salt is random data that is used as an additional input to a one-way function that hashes data, a password or passphrase.
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
 
-        newUser.password = hash;
-      });
-    });
+          newUser.password = hash;
 
-    newUser.save().then(user => {
-      jwt.sign({id: user.id}, JWT_SECRET, {expiresIn: 3600}, (err, token) => {
-        if (err) throw err;
-        res.json({
-          token,
-          user: {
-            id: user.id,
-            name: user.name,
-            email: user.email,
-          },
+          newUser.save().then(user => {
+            jwt.sign(
+              {id: user.id},
+              JWT_SECRET,
+              {expiresIn: 3600},
+              (err, token) => {
+                if (err) throw err;
+                res.json({
+                  token,
+                  user: {
+                    id: user.id,
+                    name: user.name,
+                    email: user.email,
+                  },
+                });
+              },
+            );
+          });
         });
       });
     });
